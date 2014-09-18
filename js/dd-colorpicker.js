3d5fa26dfe6e602c4fd403819c746210db065236
Davide Orazio Montersino <davide@davidemontersino.com>
2014-09-18 16:11:33 +0200

    Changed structure to emulate a constructor; other architectural enhancements

--- js/dd-colorpicker.js
+++ js/dd-colorpicker.js
@@ -1,58 +1,56 @@
-/* global console, d3 */
+/* global d3 */
 /* exported DDColorPicker*/
-function DDColorPicker(method, settings){
+function DDColorPicker(method){
 	'use strict';
 
-	this.settings = settings;
-	this.init = function(){
+	this.settings = {
+		height: 420,
+		width: 820,
+		rows: 35,
+		margin: 20,
+	};
 
-		var nA = settings.data;
+	this.extend = function(dst, src){
+		for (var attrname in src) { dst[attrname] = src[attrname]; }
+	};
 
-		var height = 420,
-			width = 820,
-			rows = 35,
-			cols = nA.length / rows,
-			margin = 20,
-			rowHeight = (height-margin*2) / rows,
-			colWidth = (width-margin*2) / cols;
+	//constructor pattern from http://stackoverflow.com/questions/1114024/constructors-in-javascript-objects
+	(function(that, settings){
+		that.extend(that.settings, settings);
+		var nA = that.settings.data;
 
-		/*
-		console.dir(this.settings.data);
-		console.dir({
-			height: height,
-			width: width,
-			rows: rows,
-			cols: cols,
-			rowHeight: rowHeight,
-			colWidth: colWidth
-		});
-	*/
+		that.settings.cols = nA.length / that.settings.rows;
+		that.settings.rowHeight = (that.settings.height - that.settings.margin*2) / that.settings.rows;
+		that.settings.colWidth = (that.settings.width - that.settings.margin * 2) / that.settings.cols;
 
-		var svg = d3.select(this.settings.elem)
+		var $ddColorPicker = that;
+
+		that.svg = d3.select(that.settings.elem)
 			.append("svg")
-			.attr('width',width)
-			.attr('height',height);
-		var g = svg.append("g");
+			.attr('width',that.settings.width)
+			.attr('height',that.settings.height);
+		
+		that.g = that.svg.append("g");
 		
 		
-		var rect = g.selectAll("rect")
+		var rect = that.g.selectAll("rect")
 			.data(nA);
 		
 		// Enter…
 		rect.enter().append("rect")
-		    .attr("width", colWidth)
-		    .attr("height", rowHeight);
+		    .attr("width", that.settings.colWidth)
+		    .attr("height", that.settings.rowHeight);
 			
 
 		// Update..
 		var updateRect = function(rectangle){
 			rectangle
 			.style("fill", function(data){return d3.rgb(data.rgb[0],data.rgb[1],data.rgb[2]);})
-		    .attr("x", function(d,index){return (Math.floor(index/rows) * colWidth) + margin; })
-			.attr("y", function(d,index){return ( (index % rows) * rowHeight) + margin; })
-			.style('stroke-width',function(d){return (d.selected === undefined | d.selected === false) ? '0' : '1'})
+		    .attr("x", function(d,index){return (Math.floor(index/that.settings.rows) * that.settings.colWidth) + that.settings.margin; })
+			.attr("y", function(d,index){return ( (index % that.settings.rows) * that.settings.rowHeight) + that.settings.margin; })
+			.style('stroke-width',function(d){return (d.selected === undefined | d.selected === false) ? '0' : '1';})
 			.style('stroke',d3.rgb(0,0,0));	
-		}
+		};
 		
 		updateRect(rect);
 
@@ -64,38 +62,36 @@ function DDColorPicker(method, settings){
 			var data = d3.select(this).data();
 
 			//Update the color name text
-			var text = svg.selectAll("#colorNameText")
+			var text = $ddColorPicker.svg.selectAll("#colorNameText")
 				.data(data)
-				.text(function(d){return d.fullname})
-				.attr('fill',function(d){return d.Lab[0] > 50 ? d3.rgb(4,4,4) : d3.rgb(251,251,251)});
+				.text(function(d){return d.fullname;})
+				.attr('fill',function(d){return d.Lab[0] > 50 ? d3.rgb(4,4,4) : d3.rgb(251,251,251);});
 
 			//Create the text element
 			text
 				.enter()
 				.append('text')
 				.attr('id', "colorNameText")
-				.attr('x',width - margin)
+				.attr('x',that.settings.width - that.settings.margin)
 				.attr('y',15)
 				.attr("font-size", "10px")
 				.attr('text-anchor','end');
 
 			//Update the background
-			var div = svg.selectAll('#colorSelected')
+			var div = $ddColorPicker.svg.selectAll('#colorSelected')
 				.data(data)
-				.style('fill',function(d){return d3.rgb(d.rgb[0],d.rgb[1],d.rgb[2]);})
-				.html(function(d){return d.fullname});
+				.style('fill',function(d){return d3.rgb(d.rgb[0],d.rgb[1],d.rgb[2]);});
 			
 			//Create the background
 			div.enter()
 				.insert('rect',':first-child')
 				.attr('id','colorSelected')
-				.attr('height',height)
-				.attr('width',width)
+				.attr('height',that.settings.height)
+				.attr('width',that.settings.width);
 		});
-		var $ddColorPicker = this;
+		
 
-		rect.on('click',function(d,i){
-			var $this = d3.select(this);
+		rect.on('click',function(d){
 			
 			d.selected = d.selected === undefined ? true : !d.selected;
 			updateRect(rect);
@@ -105,12 +101,15 @@ function DDColorPicker(method, settings){
 			}
 		});
 		
-	}
+	}(this,method));
 
 	this.selectedData = function(){
 		return this.settings.data.filter(function(item){return item.selected; });
-	}
+	};
 
 	//execute calles method
-	this[method]();
+	
+	if(typeof(method) === "string"){
+	 this[method]();
+	}
 }
